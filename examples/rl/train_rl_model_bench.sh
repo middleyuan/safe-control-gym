53dce2fcd03d7df80f73d56ca3704e4909252a03
Shambhuraj Sawant <shambhurajsawant10@gmail.com>
2025-07-16 15:10:12 +0200

    updated train rl script and final hand tuned ppo config

--- examples/rl/train_rl_model_bench.sh
+++ examples/rl/train_rl_model_bench.sh
@@ -6,33 +6,33 @@ SYS='quadrotor_2D_attitude'
 # SYS='quadrotor_3D'
 # SYS='quadrotor_3D_attitude'
 
-# TASK='stab'
-TASK='track'
-
-ALGO='ppo'
-# ALGO='dppo'
-# ALGO='sac'
-# ALGO='safe_explorer_ppo'
-
-EXP_NAME='final'
-
 if [ "$SYS" == 'cartpole' ]; then
     SYS_NAME=$SYS
 else
     SYS_NAME='quadrotor'
 fi
 
+# TASK='stab'
+TASK='track'
+
+ALGO='ppo'
+# ALGO='dppo'
+ALGO='sac'
+# ALGO='safe_explorer_ppo'
+
+EXP_NAME='final6'
+
 # TRAIN_LIST=('nominal' 'generalization' 'robustness_pm')
 # TRAIN_LIST=('robustness_ob5' 'robustness_ps3' 'robustness_combo')
-# TRAIN_LIST=('nominal' 'generalization' 'robustness_pm' 'robustness_ob5' 'robustness_ps3' 'robustness_combo' 'robustness_dw')
-TRAIN_LIST=('nominal' 'generalization' 'robustness_combo')
+# TRAIN_LIST=('nominal' 'generalization' 'robustness_pm' 'robustness_ob5' 'robustness_ps3')
+TRAIN_LIST=('nominal' 'generalization' 'robustness_pm' 'robustness_ob5' 'robustness_ps3' 'robustness_combo')
 # shellcheck disable=SC2054
-Q=(3.0,0.1,3.0,0.1,0.1,0.001)
-# Q=(10.0,0.1,10.0,0.1,0.1,0.001)
+Q=(5.0,0.1,5.0,0.1,0.1,0.001)
+# Q=(10.0,0.1,1.0,0.1,0.1,0.001)
 NS=1
 T=11
 H=0
-EVAL_LIST=('performance' 'robustness_ob' 'robustness_ps' 'robustness_pm' 'robustness_dw' 'generalization')
+EVAL_LIST=('performance' 'generalization' 'robustness_ob' 'robustness_ps' 'robustness_pm')
 
 # Train the unsafe controller/agent.
 for TRAIN in "${TRAIN_LIST[@]}"; do
@@ -43,29 +43,29 @@ for TRAIN in "${TRAIN_LIST[@]}"; do
         CONFIG1="./config_overrides/${SYS}/${ALGO}_${SYS}.yaml"
         CONFIG2="./config_overrides/${SYS}/${SYS}_${TASK}.yaml"
     elif [ "${TRAIN}" == 'generalization' ]; then
-        CONFIG1="./config_overrides/${SYS}/${ALGO}_${SYS}.yaml"
+        CONFIG1="./config_overrides/${SYS}/${ALGO}_${SYS}_gen.yaml"
         CONFIG2="./config_overrides/${SYS}/${SYS}_${TASK}_gen.yaml"
     elif [ "${TRAIN}" == 'robustness_ob5' ]; then
-        CONFIG1="./config_overrides/${SYS}/${ALGO}_${SYS}.yaml"
+        CONFIG1="./config_overrides/${SYS}/${ALGO}_${SYS}_dr.yaml"
         CONFIG2="./config_overrides/${SYS}/${SYS}_${TASK}_ob5.yaml"
     elif [ "${TRAIN}" == 'robustness_ps3' ]; then
-        CONFIG1="./config_overrides/${SYS}/${ALGO}_${SYS}.yaml"
+        CONFIG1="./config_overrides/${SYS}/${ALGO}_${SYS}_dr.yaml"
         CONFIG2="./config_overrides/${SYS}/${SYS}_${TASK}_ps3.yaml"
     elif [ "${TRAIN}" == 'robustness_ob5ps3' ]; then
-        CONFIG1="./config_overrides/${SYS}/${ALGO}_${SYS}.yaml"
-        CONFIG2="./config_overrides/${SYS}/${SYS}_${TASK}_ob5ps3.yaml"
-    elif [ "${TRAIN}" == 'robustness_combo' ]; then
-        # shellcheck disable=SC2054
-        Q=(1.0,0.1,1.0,0.1,0.1,0.001)
         CONFIG1="./config_overrides/${SYS}/${ALGO}_${SYS}_dr.yaml"
-        CONFIG2="./config_overrides/${SYS}/${SYS}_${TASK}_combo.yaml"
+        CONFIG2="./config_overrides/${SYS}/${SYS}_${TASK}_ob5ps3.yaml"
     elif [ "${TRAIN}" == 'robustness_pm' ]; then
         # shellcheck disable=SC2054
         Q=(1.0,0.1,1.0,0.1,0.1,0.001)
         CONFIG1="./config_overrides/${SYS}/${ALGO}_${SYS}_dr.yaml"
         CONFIG2="./config_overrides/${SYS}/${SYS}_${TASK}_pm.yaml"
+    elif [ "${TRAIN}" == 'robustness_combo' ]; then
+        # shellcheck disable=SC2054
+        Q=(1.0,0.1,1.0,0.1,0.1,0.001)
+        CONFIG1="./config_overrides/${SYS}/${ALGO}_${SYS}_dr.yaml"
+        CONFIG2="./config_overrides/${SYS}/${SYS}_${TASK}_combo.yaml"
     elif [ "${TRAIN}" == 'robustness_dw' ]; then
-        CONFIG1="./config_overrides/${SYS}/${ALGO}_${SYS}.yaml"
+        CONFIG1="./config_overrides/${SYS}/${ALGO}_${SYS}_dr.yaml"
         CONFIG2="./config_overrides/${SYS}/${SYS}_${TASK}_dw.yaml"
     fi
     echo ${CONFIG1}
@@ -79,7 +79,7 @@ for TRAIN in "${TRAIN_LIST[@]}"; do
                 "${CONFIG1}" \
                 "${CONFIG2}" \
             --output_dir ./Results/${EXP_NAME}/${TRAIN} \
-            --tag ${SYS}_${ALGO}_data \
+            --tag ${SYS}_${ALGO}_data21 \
             --seed "${SEED}" \
             --use_gpu \
             --kv_overrides \
@@ -87,14 +87,17 @@ for TRAIN in "${TRAIN_LIST[@]}"; do
                 task_config.normalized_rl_action_space=False\
                 task_config.rew_state_weight=${Q}
 
+        # Evaluate the trained controller/agent.
         # RL Experiment
+        wait
         for EVAL in "${EVAL_LIST[@]}"; do
+            echo "Evaluating ${EVAL} for ${SYS}_${ALGO} with seed ${SEED}"
             if [ "${EVAL}" == 'robustness_ob' ]; then
                 EXTERNAL_PARAM=(0 1 2 3 4 5 6 7 8 9 10 12 14 16 18 20 25 30 35 40 45 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200)
             elif [ "${EVAL}" == 'robustness_ps' ]; then
                 EXTERNAL_PARAM=(0 1 2 3 4 5 6 7 8 9 10 12 14 16 18 20 25 30 35 40 45 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200)
             elif [ "${EVAL}" == 'robustness_pm' ]; then
-                EXTERNAL_PARAM=(0 0.01 0.02 0.05 0.1 0.2 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0 3.5 4.0 4.5 5.0)
+                EXTERNAL_PARAM=(0 0.05 0.1 0.5 1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0 6.0 7.0 8.0 9.0 10.0 11.0 12.0 13.0 14.0 15.0)
             elif [ "${EVAL}" == 'robustness_dw' ]; then
                 EXTERNAL_PARAM=(1.5 1.75 2.0 2.25 2.5 2.75 3.0 3.5 4.0 4.5 5.0)
             elif [ "${EVAL}" == 'generalization' ]; then
@@ -104,13 +107,13 @@ for TRAIN in "${TRAIN_LIST[@]}"; do
             fi
 
             for EP in "${EXTERNAL_PARAM[@]}"; do
-                python3 ./rlmpc_experiment.py \
+                python3 ./rl_experiment.py \
                     --task ${SYS_NAME} \
                     --algo ${ALGO} \
                     --use_gpu \
                     --overrides \
                         ./config_overrides/${SYS}/${SYS}_${TASK}.yaml \
-                        ./config_overrides/${SYS}/${ALGO}_${SYS}.yaml \
+                        "${CONFIG1}" \
                     --experiment_type ${EVAL} \
                     --seed ${SEED} \
                     --kv_overrides \
@@ -123,6 +126,7 @@ for TRAIN in "${TRAIN_LIST[@]}"; do
                         task_config.external_param=${EP} \
                     --pretrain_path ./Results/${EXP_NAME}/${TRAIN}/${SYS}_${ALGO}_data/seed${SEED}_*/
             done
+            wait
         done
     done
     wait
