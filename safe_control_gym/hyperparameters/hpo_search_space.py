5665b66964d2889d5ce4a64d0673c6c618cec078
Mingxuan Che <mxche2019@outlook.com>
2025-08-19 19:29:06 +0200

    update HP search space

--- safe_control_gym/hyperparameters/hpo_search_space.py
+++ safe_control_gym/hyperparameters/hpo_search_space.py
@@ -169,19 +169,19 @@ GPMPC_TP_dict = {
     'optimization_iterations': {'values': [500, 1000, 1500, 2000, 2500, 3000], 'scale': 'uniform', 'type': int, 'cat': 'discrete'},  # type belongs to int due to 1 DoF
     'learning_rate': {'values': [5e-4, 0.5], 'scale': 'log', 'type': float, 'cat': 'float'},  # type belongs to float due to 1 DoF
     'q_mpc': {'values': [0.0001, 20], 'scale': 'uniform', 'type': list, 'cat': 'float'},
-    'r_mpc': {'values': [0.0001, 5], 'scale': 'uniform', 'type': list, 'cat': 'float'},
+    'r_mpc': {'values': [1, 5], 'scale': 'uniform', 'type': list, 'cat': 'float'},
 }
 
 LMPC_dict = {
     'horizon': {'values': [15, 20, 25, 30, 35, 40, 45, 50], 'scale': 'uniform', 'type': int, 'cat': 'discrete'},
     'q_mpc': {'values': [0.0001, 20], 'scale': 'uniform', 'type': list, 'cat': 'float'},
-    'r_mpc': {'values': [0.0001, 5], 'scale': 'uniform', 'type': list, 'cat': 'float'},
+    'r_mpc': {'values': [1, 5], 'scale': 'uniform', 'type': list, 'cat': 'float'},
 }
 
 MPC_dict = {
     'horizon': {'values': [15, 20, 25, 30, 35, 40, 45, 50], 'scale': 'uniform', 'type': int, 'cat': 'discrete'},
     'q_mpc': {'values': [0.0001, 20], 'scale': 'uniform', 'type': list, 'cat': 'float'},
-    'r_mpc': {'values': [0.0001, 5], 'scale': 'uniform', 'type': list, 'cat': 'float'},
+    'r_mpc': {'values': [1, 5], 'scale': 'uniform', 'type': list, 'cat': 'float'},
 }
 
 FMPC_dict = {
@@ -194,7 +194,7 @@ FMPC_dict = {
 
 LQR_dict = {
     'q_lqr': {'values': [0.0001, 20], 'scale': 'uniform', 'type': list, 'cat': 'float'},
-    'r_lqr': {'values': [0.0001, 5], 'scale': 'uniform', 'type': list, 'cat': 'float'},
+    'r_lqr': {'values': [1, 5], 'scale': 'uniform', 'type': list, 'cat': 'float'},
 }
 
 iLQR_dict = {
@@ -203,7 +203,7 @@ iLQR_dict = {
     'lamb_max': {'values': [1000, 1500, 2000, 2500, 3000], 'scale': 'uniform', 'type': int, 'cat': 'discrete'},
     'epsilon': {'values': [0.0001, 0.0005, 0.001, 0.005, 0.01], 'scale': 'uniform', 'type': float, 'cat': 'discrete'},
     'q_lqr': {'values': [0.0001, 20], 'scale': 'uniform', 'type': list, 'cat': 'float'},
-    'r_lqr': {'values': [0.0001, 5], 'scale': 'uniform', 'type': list, 'cat': 'float'},
+    'r_lqr': {'values': [1, 5], 'scale': 'uniform', 'type': list, 'cat': 'float'},
 }
 
 iLQR_SF_dict = {
@@ -224,9 +224,6 @@ PID_dict = {
     'p_coeff_for': {'values': [0.001, 5], 'scale': 'uniform', 'type': list, 'cat': 'float'},
     'i_coeff_for': {'values': [0.001, 5], 'scale': 'uniform', 'type': list, 'cat': 'float'},
     'd_coeff_for': {'values': [0.001, 5], 'scale': 'uniform', 'type': list, 'cat': 'float'},
-    'p_coeff_tor': {'values': [50000, 80000], 'scale': 'uniform', 'type': list, 'cat': 'float'},
-    'i_coeff_tor': {'values': [0, 700], 'scale': 'uniform', 'type': list, 'cat': 'float'},
-    'd_coeff_tor': {'values': [10000, 20000], 'scale': 'uniform', 'type': list, 'cat': 'float'},
 }
 
 
