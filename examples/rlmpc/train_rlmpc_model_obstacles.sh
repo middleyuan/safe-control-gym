81338753a6d2ae57fe63f6f666cd185a146ddf8b
Shambhuraj Sawant <shambhurajsawant10@gmail.com>
2026-07-10 08:42:13 +0200

    rlmpc config updates

--- examples/rlmpc/train_rlmpc_model_obstacles.sh
+++ examples/rlmpc/train_rlmpc_model_obstacles.sh
@@ -2,8 +2,8 @@
 
 # SYS='cartpole'
 # SYS='quadrotor_2D'
-SYS='quadrotor_2D_attitude'
-# SYS='quadrotor_3D_attitude'
+# SYS='quadrotor_2D_attitude'
+SYS='quadrotor_3D_attitude'
 
 # TASK='stab'
 TASK='track'
@@ -12,7 +12,7 @@ ALGO='ppo_mpc'
 # ALGO='appo_mpc'
 # ALGO='sac_mpc'
 
-EXP_NAME='test'
+EXP_NAME='quad_3d'
 
 if [ "$SYS" == 'cartpole' ]; then
     SYS_NAME=$SYS
@@ -21,11 +21,12 @@ else
 fi
 
 # Train the unsafe controller/agent.
-for SEED in {0..2}
+for SEED in {0..0}
 do
     for LEN in {13,15,20}
     do
         echo "Training ${ALGO} on ${SYS_NAME} for data length ${LEN} with seed ${SEED}"
+        TAG="obstacle_course/${SYS}_${ALGO}_${LEN}"
         python3 ../../safe_control_gym/experiments/train_rl_controller.py \
             --algo ${ALGO} \
             --task ${SYS_NAME} \
@@ -33,9 +34,11 @@ do
                 ./config_overrides/${SYS}/${ALGO}_${SYS}_${LEN}.yaml \
                 ./config_overrides/${SYS}/obstacle_course_${LEN}.yaml \
             --output_dir ./Results/${EXP_NAME} \
-            --tag ${SYS}_${ALGO}_data4_${LEN} \
+            --tag ${TAG} \
             --seed ${SEED} \
             --kv_overrides \
                 task_config.randomized_init=True
     done
+    wait
+    # tb-reducer ./Results/${EXP_NAME}/${TAG}/seed* -o ./Results/${EXP_NAME}/${TAG}/ -r mean
 done
